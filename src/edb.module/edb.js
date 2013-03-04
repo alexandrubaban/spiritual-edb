eebf948f01ff72f22c64e7897e9939a0da08c69e
Wired Earp <wiredearp@gmail.com>
2013-03-04 11:31:16 +0100

    Explicit global ns + missing broadcast.

--- src/edb.module/edb.js
+++ src/edb.module/edb.js
@@ -1,7 +1,7 @@
 /*
  * Namepace object.
  */
-var edb = gui.namespace ( "edb", {
+window.edb = gui.namespace ( "edb", {
 
 	/**
 	 * Identification.
@@ -12,8 +12,9 @@ var edb = gui.namespace ( "edb", {
 	},
 
 	BROADCAST_FUNCTION_LOADED : "broadcast-function-loaded",
+	BROADCAST_TAG_LOADED : "broadcast-tag-loaded",
 	LIFE_SCRIPT_WILL_RUN : "life-script-will-run",
-	LIFE_SCRIPT_DID_RUN	: "life-script-did-run",
+	LIFE_SCRIPT_DID_RUN : "life-script-did-run",
 	TICK_SCRIPT_UPDATE : "gui-tick-spiritscript-update",
 	TICK_COLLECT_INPUT : "gui-tick-collect-input"
 

f27ae89edb52444c4badbd0447c2a10f27e426d4
Wired Earp <wiredearp@gmail.com>
2013-03-15 07:32:48 +0100

    Preliminary compiler refactoring.

---
+++ src/edb.module/scripts/utils/edb.Result.js
@@ -0,0 +1,6 @@
+edb.Result = function ( text ) {
+	this.body = text || "";
+};
+edb.Result.prototype = {
+	body : null
+};
---
+++ src/edb.module/scripts/utils/edb.Status.js
@@ -0,0 +1,34 @@
+/**
+ * Tracking compiler state.
+ * @todo Comments all over.
+ * @param {String} body
+ */
+edb.Status = function ( body ) {
+	this.body = body || "";
+	this.conf = [];
+};
+
+edb.Status.prototype = {
+	mode : edb.Status.MODE_JS,
+	body : null,
+	peek : false,
+	poke : false,
+	cont : false,
+	adds : false,
+	func : null,
+	conf : null,
+	skip : 0,
+	last : 0,
+	spot : 0,
+	indx : 0,
+
+	// tags
+	refs : false // pass by reference in tags
+};
+
+
+// Static ..........................
+
+edb.Status.MODE_JS = "js";
+edb.Status.MODE_HTML = "html";
+edb.Status.MODE_TAG = "tag";
